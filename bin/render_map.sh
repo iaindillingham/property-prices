31da1f5856f003faf2b335a15947d805c9378d36
Iain Dillingham <iain@dillingham.me.uk>
2020-12-06 20:35:42 +0000

    Add render_map stage

---
+++ bin/render_map.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DST_DIR=reports/figures
+
+# https://medium.com/@adamhooper/fonts-in-node-canvas-bbf0b6b0cabf
+PANGOCAIRO_BACKEND=fontconfig
+
+mkdir -p $DST_DIR
+vg2png -b models/ specs/map.vg.json $DST_DIR/map.png
+vg2pdf -b models/ specs/map.vg.json $DST_DIR/map.pdf
